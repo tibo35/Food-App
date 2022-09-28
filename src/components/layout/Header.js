1c554daa677d7e04d53fbf2a70ce1d6ee6924147
thibaud aptel <thibaudaptel@thibauds-MBP.modem>
2022-09-28 11:51:00 +1000

    first commit

---
+++ src/components/layout/Header.js
@@ -0,0 +1,20 @@
+import React, { Fragment } from "react";
+
+import HeaderCartButton from "./HeaderCartButton";
+import mealsImg from "../../assets/meals.jpeg";
+import classes from "./Header.module.css";
+
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>Meals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImg} alt="table of food" />
+      </div>
+    </Fragment>
+  );
+};
+export default Header;
