3518f75da184aceb64f2bd2a8545fff2affe5ca6
thibaud aptel <thibaudaptel@thibauds-MBP.modem>
2022-09-29 15:21:03 +1000

    modal component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
+
 import Header from "./components/layout/Header";
 import Meals from "./components/meals/Meals";
-
+import Cart from "./components/cart/Cart";
 import "./App.css";
 
 function App() {
+  const [cartIsVisible, setCartIsVisible] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsVisible(true);
+  };
+  const hideCartHandler = () => {
+    setCartIsVisible(false);
+  };
+
   return (
     <Fragment>
-      <Header />
+      {cartIsVisible && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
