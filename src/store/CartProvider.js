1d96651900c3598fa140c9e1c191c341e9425a73
thibaud aptel <thibaudaptel@thibauds-MBP.modem>
2022-10-19 12:12:05 +1100

    cart Context refresh

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -54,6 +54,10 @@ const cartReducer = (state, action) => {
     };
   }
 
+  if (action.type === "CLEAR") {
+    return defaultCartState;
+  }
+
   return defaultCartState;
 };
 const CartProvider = (props) => {
@@ -68,11 +72,15 @@ const CartProvider = (props) => {
   const removeItemFromCartHandler = (id) => {
     dispatchCartAction({ type: "REMOVE", id: id });
   };
+  const clearCartHandler = () => {
+    dispatchCartAction({ type: "CLEAR" });
+  };
   const cartContext = {
     items: cartState.items,
     totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
+    clearCart: clearCartHandler,
   };
   return (
     <CartContext.Provider value={cartContext}>
